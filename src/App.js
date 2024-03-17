87ccce9fb9053de2ee7c643758f745057a8bc31d
Harsh <harshprsh.14@gmail.com>
2024-03-17 13:10:56 +0530

    Write assignment logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ReactApexChart from 'react-apexcharts';
 
-function App() {
+const App = () => {
+  const [requests, setRequests] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://checkinn.co/api/v1/int/requests');
+      console.log('API Response:', response.data.requests);
+      setRequests(response.data.requests);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  
+  const getRequestsPerHotel = () => {
+    const requestsPerHotel = {};
+
+    requests.forEach(request => {
+      const hotelName = request.hotel.name;
+      if (requestsPerHotel[hotelName]) {
+        requestsPerHotel[hotelName]++;
+      } else {
+        requestsPerHotel[hotelName] = 1;
+      }
+    });
+
+    return requestsPerHotel;
+  };
+
+  const chartOptions = {
+    xaxis: {
+      categories: Object.keys(getRequestsPerHotel()),
+    },
+  };
+
+  const chartSeries = [
+    {
+      name: 'Number of Requests',
+      data: Object.values(getRequestsPerHotel()),
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Requests per hotel</h1>
+      {Object.keys(getRequestsPerHotel()).length > 0 ? (
+        <ReactApexChart options={chartOptions} series={chartSeries} type="line" height={350} />
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
